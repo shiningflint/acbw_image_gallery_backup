a392df1270945305e8459bc04add46d906278317
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-09-08 18:43:22 +0900

    environment variable fix 2

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update({
-  region: '<%= ENV["AWS_S3_REGION"] %>',
-  credentials: Aws::Credentials.new('<%= ENV["AWS_S3_ID"] %>', '<%= ENV["AWS_S3_KEY"] %>')
+  region: ENV['AWS_S3_REGION'],
+  credentials: Aws::Credentials.new(ENV['AWS_S3_ID'], ENV['AWS_S3_KEY'])
 })
 
 # uncomment below when testing with windows
